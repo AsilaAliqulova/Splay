7a6815fcb4cfb4a6aaf000bf0581d5383c3873db
Asila Aliqulova <asilaaliqulova@gmail.com>
2025-03-05 13:26:57 +0500

    add common

---
+++ src/common/strategies/refresh-token-cookie.strategy.ts
@@ -0,0 +1,31 @@
+import { ForbiddenException, Injectable } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+import { JwtFromRequestFunction, Strategy } from "passport-jwt";
+import { JwtPayload, JwtPayloadWithRefreshToken } from "../types";
+import { Request } from "express"; 
+
+
+export const coookieExtractor:JwtFromRequestFunction=(req:Request)=>{
+    if(req && req.cookies){
+        return req.cookies["refresh_token"]
+    }
+    return null
+}
+@Injectable()
+export class RefreshTokenCookiesStrategy extends PassportStrategy(Strategy,"refresh-jwt"){
+    constructor(){
+        super({
+            jwtFromRequest:coookieExtractor,
+            secretOrKey:process.env.REFRESH_TOKEN_KEY!,
+            passReqToCallback:true
+        })
+    }
+    validate(req:Request,payload:JwtPayload): JwtPayloadWithRefreshToken {
+        const refreshToken= req.cookies.refresh_token
+        if(!refreshToken){
+            throw new ForbiddenException("Refresh token noto'g'ri")
+
+        }
+        return {...payload, refreshToken}
+    }
+}
